95d5454a8136baf85d791f8d89ad0ba71da8ae53
petersoneannaluize-creator <petersone.annaluize@gmail.com>
2026-06-01 11:33:23 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -409,7 +409,7 @@ export default function App() {
             </p>
 
             <p className="lead smaller">
-              Soļu kopskaits par soļu kopskaitu no pirmdienas līdz svētdienai jāiesniedz līdz katras pirmdienas plkst. 11.00, lai pirmdienās Regulatora komunikācijas speciāliste Anna var šeit atjaunot kopējo progresu.
+              Soļu kopskaits ir jāiesniedz līdz katras pirmdienas plkst. 11.00, lai Regulatora komunikācijas speciāliste Anna var šeit pirmdienās atjaunot kopējo progresu.
               Ja ar to vajag palīdzību, droši raksti vai ziņo Annai, un viņa ar prieku palīdzēs! Epasts saziņai: anna.petersone@sprk.gov.lv vai meklē Teams Anna Pētersone.
             </p>
           </div>
